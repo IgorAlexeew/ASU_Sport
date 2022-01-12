2ec43218ff50721be917e692ea2a4619f57057b4
IgorAlexeew <goscha.alekseew2015@yandex.ru>
2022-01-12 11:33:04 +0400

    Исправление регистрация (фронт)

--- wwwroot/js/authentication.js
+++ wwwroot/js/authentication.js
@@ -1,11 +1,8 @@
-let password_field = $('input[name="password"]'),
+/*let password_field = $('input[name="password"]'),
     confirm_password_field = $('input[name="confirm_password"]'),
     login_field = $('input[name="login"]');
-access_code_field = $('input[name="access_code"]');
 
-console.log(access_code_field.val());
-
-/* Авторизация */
+/!* Авторизация *!/
 $('.login-btn').on("click", function (event) {
     event.preventDefault();
 
@@ -23,10 +20,10 @@ $('.login-btn').on("click", function (event) {
             login: login,
             password: password
         }),
-        /*headers: {
+        /!*headers: {
             RequestVerificationToken:
                 $('input:hidden[name="__RequestVerificationToken"]').val()
-        },*/
+        },*!/
         success (data) {
             console.log(data);
             if (data.status)
@@ -67,11 +64,11 @@ $('.confirm-btn').on("click", function (event) {
         login_field.addClass('error');
         has_error = true;
     }
-    /*if (access_code_field.val() === '')
+    /!*if (access_code_field.val() === '')
     {
         access_code_field.addClass('error');
         has_error = true;
-    }*/
+    }*!/
     if (password_field.val() === '')
     {
         password_field.addClass('error');
@@ -114,62 +111,218 @@ $('.confirm-btn').on("click", function (event) {
         })
     }
 
-});
+});*/
 
-function password_check(){
-    $('#password_match').addClass('animated');
-    if (password_field.val() === confirm_password_field.val() && password_field.val() !== '') {
-        confirm_password_field.removeClass('error').addClass('passed')
-        password_field.removeClass('error').addClass('passed');
-        $('p#password_match').removeClass('error').addClass('passed').addClass('hidden').text('Пароли совпадают');
-    }
-    else if (confirm_password_field.val() === '')
-    {
-        confirm_password_field.removeClass('error').removeClass('passed')
-        password_field.removeClass('error').removeClass('passed');
-        $('p#password_match').addClass('hidden');
-    }
-    else
-    {
-        confirm_password_field.removeClass('passed').addClass('error');
-        password_field.removeClass('passed').addClass('error');
-        $('p#password_match').text((password_field.val() !== '') ? 'Пароли не совпадают' : 'Введите пароль').removeClass('hidden').removeClass('passed').addClass('error');
-    }
-}
+import {loader} from "./shared-components.js";
 
-password_field.change(password_check);
-confirm_password_field.change(password_check);
+const app = Vue.createApp({})
 
-function login_check()
-{
-    // let login_regexp = /^([a-zA-Z0-9]+[-_]?[a-zA-Z0-9]+)+$/;
-    let login_regexp = /^[a-zA-Z][a-zA-Z0-9-_.]{1,20}$/;
-    let login = login_field.val();
-
-    if (login !== '')
-    {
-        if (!login_regexp.test(login))
-        {
-            login_field.addClass('error').removeClass('passed');
-            $('p#login_match').addClass('error').removeClass('hidden').text('Неверный формат логина');
+app.component("sign-up-form", {
+    data() {
+        return {
+            form: {
+                login: '',
+                password: '',
+                lastName: '',
+                firstName: '',
+                middleName: '',
+                dateOfBirth: '',
+                phoneNumber: ''
+            },
+            confirm_password: '',
+            password_classes: [],
+            login_classes: [],
+            last_name_classes: [],
+            first_name_classes: [],
+            middle_name_classes: [],
+            date_of_birth_classes: [],
+            phone_number_classes: [],
+            login_message_classes: ['hidden'],
+            password_message_classes: ['hidden'],
+            login_message: "",
+            password_message: "",
+            isLoading: false
         }
-        else
-        {
-            login_field.addClass('passed').removeClass('error');
-            $('p#login_match').addClass('hidden');
+    },
+    watch: {
+        form: {
+            handler(val, oldVal) {
+                this.form_check()
+            },
+            deep: true
+        },
+        confirm_password(val) {
+            this.password_check()
         }
-    }
-    else
-    {
-        login_field.removeClass('passed').removeClass('error');
-        $('p#login_match').addClass('hidden');
-    }
-}
+    },
+    methods: {
+        login_check()
+        {
+            // let login_regexp = /^([a-zA-Z0-9]+[-_]?[a-zA-Z0-9]+)+$/;
+            let login_regexp = /^[a-zA-Z][a-zA-Z0-9-_.]{1,20}$/;
+            this.is_login_correct = login_regexp.test(this.form.login);
+            if (login_regexp.test(this.form.login)) {
+                this.login_classes = ['passed']
+                this.login_message = "Все ок"
+                this.login_message_classes = ["passed", "hidden"]
+            } else if (this.form.login === '') {
+                this.login_classes = []
+                this.login_message = "Поле логина пустое"
+                this.login_message_classes = ["hidden"]
+            } else {
+                this.login_classes = ['error']
+                this.login_message = "Неверный формат"
+                this.login_message_classes = ["error"]
+            }
+        },
+        password_check() {
+            if (this.form.password === this.confirm_password && this.form.password.length > 3) {
+                this.password_classes = ['passed']
+                this.password_message = "Все ок"
+                this.password_message_classes = ["passed", "hidden"]
+            } else if (this.form.password === this.confirm_password && this.form.password === '') {
+                this.password_classes = []
+                this.password_message = "Поля паролей пустые"
+            } else {
+                this.password_classes = ['error']
+                if (this.form.password !== this.confirm_password) {
+                    this.password_message = "Пароли не совпадают"
+                    this.password_message_classes = ["error"]
+                } else {
+                    this.password_message = "Пароль слишком короткий"
+                    this.password_message_classes = ["error"]
+                }
+            }
+        },
+        form_check() {
+            this.login_check()
+            this.password_check()
+            this.last_name_classes = []
+            this.first_name_classes = []
+            this.middle_name_classes = []
+            this.date_of_birth_classes = []
+            this.phone_number_classes = []
+        },
+        submit() {
+            let error = false
+            if (this.form.login === '') {
+                this.login_classes = ['error']
+                error = true
+            }
+            if (this.form.password === '') {
+                this.password_classes = ['error']
+                error = true
+            }
+            if (this.form.lastName === '') {
+                this.last_name_classes = ['error']
+                error = true
+            }
+            if (this.form.firstName === '') {
+                this.first_name_classes = ['error']
+                error = true
+            }
+            if (this.form.middleName === '') {
+                this.middle_name_classes = ['error']
+                error = true
+            }
+            if (this.form.dateOfBirth === '') {
+                this.date_of_birth_classes = ['error']
+                error = true
+            }
+            if (this.form.phoneNumber === '') {
+                this.phone_number_classes = ['error']
+                error = true
+                console.log("HI")
+            }
 
-login_field.change(login_check);
-access_code_field.change(function () {
-    if (access_code_field.val() === '')
-        access_code_field.removeClass('passed');
-    else
-        access_code_field.removeClass('error').addClass('passed');
+            if (!error) {
+                this.isLoading = true
+                axios
+                    .post("/api/auth/sign-up", this.form)
+                    .then(response => {
+                        if (response.status)
+                        {
+                            this.form_check()
+                            document.location.href = '/user';
+                        }
+                        else
+                        {
+                            if (data.type === "username_is_already_taken")
+                            {
+                                this.login_classes = ['error']
+                                this.login_message_classes = ['error']
+                                this.login_message = "Логин уже занят"
+                            }
+                        }
+                        this.isLoading = false
+                    })
+            }
+        }
+    },
+    template: `
+    <form>
+        <p class="form_title">Регистрация</p>
+        <label for="login">Логин:</label>
+        <input :class="this.login_classes" type="text" name="login" id="login" v-model="this.form.login" readonly onfocus="this.removeAttribute('readonly')">
+        <p class="message" :class="this.login_message_classes" id="login_match">{{this.login_message}}</p>
+        
+        <label for="password">Пароль:</label>
+        <input :class="this.password_classes" type="password" name="password" id="password" v-model="this.form.password" readonly onfocus="this.removeAttribute('readonly')">
+        
+        <label for="confirm_password">Подтвердите пароль:</label>
+        <input :class="this.password_classes" type="password" name="confirm_password" id="confirm_password" v-model="this.confirm_password">
+        <p class="message" :class="this.password_message_classes" id="password_match">{{this.password_message}}</p>
+
+        <label for="login">Фамилия:</label>
+        <input :class="this.last_name_classes" type="text" name="last_name" id="last_name" v-model="this.form.lastName" readonly onfocus="this.removeAttribute('readonly')">
+
+        <label for="login">Имя:</label>
+        <input :class="this.first_name_classes" type="text" name="first_name" id="first_name" v-model="this.form.firstName" readonly onfocus="this.removeAttribute('readonly')">
+
+        <label for="login">Отчество:</label>
+        <input :class="this.middle_name_classes" type="text" name="middle_name" id="middle_name" v-model="this.form.middleName" readonly onfocus="this.removeAttribute('readonly')">
+
+        <label for="login">Дата рождения:</label>
+        <input :class="this.date_of_birth_classes" type="date" name="date_of_birth" id="date_of_birth" v-model="this.form.dateOfBirth" readonly onfocus="this.removeAttribute('readonly')">
+
+        <label for="login">Номер телефона:</label>
+        <input :class="this.phone_number_classes" type="tel" pattern="[+]{1}[0-9]{11,14}" name="phone_number" id="phone_number" v-model="this.form.phoneNumber" readonly onfocus="this.removeAttribute('readonly')" placeholder="+7 (999) 999-99-99">
+
+
+        <button type="submit" class="confirm-btn" @click.prevent="submit">Подтвердить</button>
+    </form>
+    <div v-if="this.isLoading" class="loader-wrap">
+        <loader></loader>
+    </div>
+    `
 })
+
+app.component("asu-header", {
+    props: [],
+    template: `
+    <header>
+        <img id="asu_logo" src="/img/asu-clr.png" alt="Астраханский Государственный Университет">
+        <div class="header_text">
+            <div class="logo">АГУ<br>СПОРТ</div>
+            <div class="title">Быстрее Выше Сильнее</div>
+        </div>
+    </header>`
+})
+
+app.component("registration-page", {
+    components: {
+        loader: loader
+    },
+    props: [],
+    template: `
+    <asu-header></asu-header>
+    <sign-up-form></sign-up-form>
+    `
+})
+
+app.mount("#app")
+
+$(function(){
+    //2. Получить элемент, к которому необходимо добавить маску
+    $("#phone_number").mask("+7 (999) 999-99-99");
+});
--- wwwroot/js/user.js
+++ wwwroot/js/user.js
@@ -179,14 +179,15 @@ app.component("user-info",{
 <!--                  <span v-if="this.$root.user.role === 'admin'" class="admin">админстратор</span>-->
                 </div>
                 <p v-if="!this.$root.isEditing" class="full-name">{{ this.$root.user.lastName }} {{ this.$root.user.firstName }} {{ this.$root.user.middleName }}</p>
-                <p class="birth-date">Дата рождения: {{ (new Date(this.$root.user?.dateOfBirth)).toLocaleDateString("ru", {day:"2-digit",month:"2-digit",year:"numeric"}) }} 
-                  <span v-if="this.$root.isEditing" class="datepicker-toggle">
+                <p class="birth-date" :class="{editing:this.$root.isEditing}">Дата рождения:&nbsp;<span v-if="!this.$root.isEditing">{{ (new Date(this.$root.user?.dateOfBirth)).toLocaleDateString("ru", {day:"2-digit",month:"2-digit",year:"numeric"}) }}</span>
+                  <input v-else type="date" v-model="this.$root.user.dateOfBirth">
+<!--                  <span v-if="this.$root.isEditing" class="datepicker-toggle">
                     <span class="datepicker-toggle-button"></span>
                     <input type="date" class="datepicker-input" v-model="this.$root.user.dateOfBirth">
-                  </span>
+                  </span>-->
                 </p>
     <!--            <p class="e-mail">dostoevskiy@poet.ru</p>-->
-                <p class="phone-number">Номер телефона: <input v-model="this.$root.user.phoneNumber" :disabled="!this.$root.isEditing"></p>
+                <p class="phone-number" :class="{editing:this.$root.isEditing}"><div>Номер телефона:&nbsp;</div><input v-model="this.$root.user.phoneNumber" :disabled="!this.$root.isEditing"></p>
             </div>
 <!--            <div class="edit">
               <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet" viewBox="0 0 576 512"><path d="M402.3 344.9l32-32c5-5 13.7-1.5 13.7 5.7V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V112c0-26.5 21.5-48 48-48h273.5c7.1 0 10.7 8.6 5.7 13.7l-32 32c-1.5 1.5-3.5 2.3-5.7 2.3H48v352h352V350.5c0-2.1.8-4.1 2.3-5.6zm156.6-201.8L296.3 405.7l-90.4 10c-26.2 2.9-48.5-19.2-45.6-45.6l10-90.4L432.9 17.1c22.9-22.9 59.9-22.9 82.7 0l43.2 43.2c22.9 22.9 22.9 60 .1 82.8zM460.1 174L402 115.9L216.2 301.8l-7.3 65.3l65.3-7.3L460.1 174zm64.8-79.7l-43.2-43.2c-4.1-4.1-10.8-4.1-14.8 0L436 82l58.1 58.1l30.9-30.9c4-4.2 4-10.8-.1-14.9z"/></svg>
